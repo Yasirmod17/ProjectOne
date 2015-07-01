28d4c265efbd2bc6f2c9b7feb83ece9b47d9a636
Yasirmod17 <mibrahim17@amherst.edu>
2015-07-01 16:31:01 +0100

    changed mongo URL

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ config = {
 	},
 	'production': {
 		mongo: {
-			url: 'mongodb://localhost/Soccer',
+			url: 'mongodb://heroku_d079jmlp:rsscua61q4uc7jm02m6h4qtvp2@ds063449.mongolab.com:63449/heroku_d079jmlp',
 			port: '27017'
 		}
 	}
